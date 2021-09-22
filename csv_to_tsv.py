32df938e460d0d121019d13a5996e317444ca7a3
Vishwajeet Dabholkar <36807939+vishwajeetdabholkar@users.noreply.github.com>
2021-09-22 22:21:31 +0530

    Update csv_to_tsv.py

--- csv_to_tsv.py
+++ csv_to_tsv.py
@@ -1,12 +1,13 @@
 import csv
 
-def csv_to_tsv(inFile, outFile):
-    with open(inFile,'r') as csvin, open(outFile, 'w', newline='') as tsvout:
-    csvin = csv.reader(csvin, skipinitialspace=True)
-    tsvout = csv.writer(tsvout, delimiter='\t',quotechar='"')
+def csv_to_tsv(inFile, outFile):    
+    # code to convert txt file to tsv file
+    with open('file 02.txt','r') as csvin, open('output02.txt', 'w', newline='') as tsvout:
+        csvin = csv.reader(csvin, skipinitialspace=True)
+        tsvout = csv.writer(tsvout, delimiter='\t',quotechar='"')
 
-    for row in csvin:
-        tsvout.writerow(row)
+        for row in csvin:
+            tsvout.writerow(row)
         
 inFile = 'file 02.txt'
 outFile = 'output02.txt'
